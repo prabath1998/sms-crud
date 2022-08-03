4ea42f3fc16fe11e049291caa888c1049341a9d3
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-03 18:21:02 +0530

    Initial commit

---
+++ src/main/java/com/test/sms/SmsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.test.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/sms/SmsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
